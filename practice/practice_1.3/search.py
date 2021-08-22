be49bc2e2df78e6603e53bafa74b5f0e9db27bf5
Vladimir <voronov.volodya2013@yandex.ru>
2021-08-22 12:39:08 +0700

    :test_tube: test for work 1.3

---
+++ practice/practice_1.3/search.py
@@ -0,0 +1,6 @@
+"""Шаблон модуля search"""
+
+
+def search(string, sub_string, case_sensitivity, method, count):
+    """Шаблон функции поиска"""
+    raise NotImplementedError()
---
+++ practice/practice_1.3/test_search.py
@@ -0,0 +1,111 @@
+"""Тесты для модуля search"""
+
+import unittest
+
+import search  # pylint: disable=E0401
+
+
+TEST_SEARCH_ONE_SYMBOL = [
+    ('', 'a', False, 'first', 1, None),
+    ('', 'a', True, 'first', 1, None),
+    ('', 'a', False, 'last', 1, None),
+    ('', 'a', True, 'last', 1, None),
+
+    ('a', 'a', False, 'first', 1, (0, )),
+    ('a', 'a', True, 'first', 1, (0, )),
+    ('a', 'a', False, 'last', 1, (0, )),
+    ('a', 'a', True, 'last', 1, (0, )),
+
+    ('aaa', 'a', False, 'first', 1, (0, 1, 2)),
+    ('aaa', 'a', True, 'first', 1, (0, 1, 2)),
+    ('aaa', 'a', False, 'last', 1, (2, 1, 0)),
+    ('aaa', 'a', True, 'last', 1, (2, 1, 0)),
+
+    ('bca', 'c', False, 'first', 1, (1, )),
+    ('bca', 'c', True, 'first', 1, (1, )),
+    ('bca', 'c', False, 'last', 1, (1, )),
+    ('bca', 'c', True, 'last', 1, (1, )),
+]
+
+TEST_SEARCH_MANY_SYMBOL = [
+    ('', 'abc', False, 'first', 1, None),
+    ('', 'abc', True, 'first', 1, None),
+    ('', 'abc', False, 'last', 1, None),
+    ('', 'abc', True, 'last', 1, None),
+
+    ('a', 'abc', False, 'first', 1, None),
+    ('a', 'abc', True, 'first', 1, None),
+    ('a', 'abc', False, 'last', 1, None),
+    ('a', 'abc', True, 'last', 1, None),
+
+    ('abc', 'abc', False, 'first', 1, (0, )),
+    ('abc', 'abc', True, 'first', 1, (0, )),
+    ('abc', 'abc', False, 'last', 1, (0, )),
+    ('abc', 'abc', True, 'last', 1, (0, )),
+
+    ('abcabc', 'abc', False, 'first', 1, (0, 3)),
+    ('abcabc', 'abc', True, 'first', 1, (0, 3)),
+    ('abcabc', 'abc', False, 'last', 1, (3, 0)),
+    ('abcabc', 'abc', True, 'last', 1, (3, 0)),
+
+    ('aabcbccaabc', 'abc', False, 'first', 1, (1, 8)),
+    ('aabcbccaabc', 'abc', True, 'first', 1, (1, 8)),
+    ('aAbCbccaabc', 'AbC', False, 'first', 1, (1, 8)),
+    ('aabcbccaAbC', 'AbC', True, 'first', 1, (8, )),
+    ('aabcbccaabc', 'abc', False, 'last', 1, (8, 1)),
+    ('aabcbccaabc', 'abc', True, 'last', 1, (8, 1)),
+    ('aAbCbccaabc', 'AbC', False, 'last', 1, (8, 1)),
+    ('aabcbccaAbC', 'AbC', True, 'last', 1, (8, )),
+]
+
+TEST_SEARCH_FEW_SUBSTR = [
+    ('', ('abc', 'a'), False, 'first', 1, None),
+    ('', ('abc', 'a'), True, 'first', 1, None),
+    ('', ('abc', 'a'), False, 'last', 1, None),
+    ('', ('abc', 'a'), True, 'last', 1, None),
+
+    ('a', ('abc', 'a'), False, 'first', 1, {'abc': None, 'a': (0, )}),
+    ('a', ('abc', 'a'), True, 'first', 1, {'abc': None, 'a': (0, )}),
+    ('a', ('abc', 'a'), False, 'last', 1, {'abc': None, 'a': (0, )}),
+    ('a', ('abc', 'a'), True, 'last', 1, {'abc': None, 'a': (0, )}),
+
+    ('ababbababa', ('aba', 'bba'), False, 'first', 1, {'aba': (0, 5, 7), 'bba': (3, )}),
+    ('ababbababa', ('aba', 'bba'), True, 'first', 1, {'aba': (0, 5, 7), 'bba': (3, )}),
+    ('ababbababa', ('aba', 'bba'), False, 'last', 1, {'aba': (7, 5, 0), 'bba': (3, )}),
+    ('ababbababa', ('aba', 'bba'), True, 'last', 1, {'aba': (7, 5, 0), 'bba': (3, )}),
+]
+
+class TestSearch(unittest.TestCase):
+    """Тест-кейс модуля search"""
+    def test_binary_search_one_symbol(self):
+        """Тест функции search для поиска строки из одного символа"""
+        for string, sub_string, case_sensitivity, method, count, expected in TEST_SEARCH_ONE_SYMBOL:
+            with self.subTest():
+                self.assertEqual(
+                    search.search(
+                        string, sub_string, case_sensitivity, method, count
+                    ),
+                    expected
+                )
+
+    def test_binary_search_many_symbol(self):
+        """Тест функции search для поиска строки из нескольких символов"""
+        for string, sub_string, case_sensitivity, method, count, expected in TEST_SEARCH_MANY_SYMBOL:
+            with self.subTest():
+                self.assertEqual(
+                    search.search(
+                        string, sub_string, case_sensitivity, method, count
+                    ),
+                    expected
+                )
+
+    def test_binary_search_few_substr(self):
+        """Тест поиска нескольких подстрок"""
+        for string, sub_string, case_sensitivity, method, count, expected in TEST_SEARCH_FEW_SUBSTR:
+            with self.subTest():
+                self.assertEqual(
+                    search.search(
+                        string, sub_string, case_sensitivity, method, count
+                    ),
+                    expected
+                )
